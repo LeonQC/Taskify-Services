1b926c5fc5efa98da7613cbb516d583818445332
ChunhuiY <146665347+ChunhuiY@users.noreply.github.com>
2024-07-07 18:00:38 -0400

    Api-project management

---
+++ Project management/myproject/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('test/', views.test_view),
+    # re_path(r'^(?P<short_key>\w{6})$', views.redirect_url, name='redirect_url'),
+]
